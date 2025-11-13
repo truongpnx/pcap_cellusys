ec13d27385ec70a1860724f890702f7abfeb88d6
kuuhakuXzy <hnguyen.nguyen01xzy@gmail.com>
2025-11-13 13:40:52 +0700

    Adjust redis variables in main.py

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ PCAP_DIRECTORIES_STR = os.getenv("PCAP_DIRECTORIES", "pcaps")
 PCAP_DIRECTORIES = [path.strip() for path in PCAP_DIRECTORIES_STR.split(',')]
 
 REDIS_HOST = os.getenv("REDIS_HOST", "localhost")
-REDIS_PORT = int(os.getenv("REDIS_PORT", 6379))
+REDIS_PORT = int(os.getenv("REDIS_INTERNAL_PORT", 6379))
 
 BASE_URL = os.getenv("BE_BASE_URL")
 BASE_PORT = os.getenv("BE_BASE_PORT")
